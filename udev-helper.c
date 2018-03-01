b1b9d5d5d1d3fbd83154b3f09c2fba53400229af
Merlijn Wajer <merlijn@wizzup.org>
2018-03-02 00:09:57 +0100

    (wip) find phy and otg controller
    Also read usb_mode, get values on start, more TODOs and notes

--- udev-helper.c
+++ udev-helper.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include <gudev/gudev.h>
 
@@ -8,73 +9,195 @@
 
 
 /*
- * /sys/class/phy/phy-48070000.i2c\:twl\@48\:twl4030-usb.0
- * /sys/class/udc/musb-hdrc.0.auto
- *
- * /sys/class/phy/phy-48070000.i2c\:twl\@48\:twl4030-usb.0/device/vbus
- * /sys/devices/platform/68000000.ocp/480ab000.usb_otg_hs/musb-hdrc.0.auto/vbus
- * /sys/devices/platform/68000000.ocp/480ab000.usb_otg_hs/musb-hdrc.0.auto/mode
  *
  * TODO:
- * - No hardcoded paths, proper device type matching
- * - Implement name getting
- * - Implement peripheral status getting (!)
- * - Remove printfs, debug code
+ * - No hardcoded driver names (whitelist?) - see find_phy and
+ *   find_otg_controller
+ * - Implement uh_get_device_name (add to cache?)
+ * - Rework callbacks to just expect a struct with usb data (cable connected,
+ *   usb_mode, ...)
+ * - Rework usb_plugin.c to use libusbgx [1]
+ * - Test on device with proper otg detection (droid4, lime2) and add/implement
+ *   USB_MODE_*
+ * - Remove usleep (and unistd.h) and replace with g_bla_callback
+ * - Figure out how to free `client`
+ * - Run through valgrind
  *
- * TODO gadget (different file in the end):
- * - Implement libusbg (or even use gadgetg)
+ * [1] https://github.com/libusbgx/libusbgx
  */
+
+#define N900_PHY_DRIVER "twl4030_usb"
+#define N900_USB_DRIVER "musb-hdrc"
 
 typedef struct {
     gboolean connected;
-
+    gint usb_mode;
 } PrivData;
 
-/* TODO: Better/non-conflicting(?) names? */
+
 static gpointer user_data = NULL;
-static UhCallback user_callback;
+static UhCallback user_callback = NULL;
 
 static GUdevClient* client = NULL;
-static PrivData cache = { .connected = 0 };
+static GUdevDevice* phy = NULL;
+static GUdevDevice* otg = NULL;
 
-static void on_uevent(GUdevClient *client, const char *action, GUdevDevice *device) {
-    (void)client;
+static PrivData cache = { .connected = 0,
+                          .usb_mode = 0 };
 
-    const gchar* subsys = g_udev_device_get_subsystem(device);
-    const gchar* devtype = g_udev_device_get_devtype(device);
-    const gchar* name = g_udev_device_get_name(device);
 
-    fprintf(stderr, "subsys: %s; devtype: %s; name: %s; action: %s\n",
-            subsys, devtype, name, action);
+static gint read_usb_mode(void) {
+    const gchar* otg_sysfs_path = g_udev_device_get_sysfs_path(otg);
+    gchar* path;
+    gchar *strmode = NULL;
+    int mode;
 
-    if (strcmp(name, "48070000.i2c:twl@48:twl4030-usb") == 0) {
-        cache.connected = (strcmp(action, "online") == 0);
+    mode = USB_MODE_UNKNOWN;
 
-        if (user_callback) {
-            user_callback(cache.connected, user_data);
+    path = g_strconcat(otg_sysfs_path, "/mode", NULL);
+    gboolean ok = g_file_get_contents(path, &strmode, NULL, NULL);
+    if (ok) {
+        if (strcmp(strmode, "b_idle\n") == 0) {
+            mode = USB_MODE_B_IDLE;
+        } else if (strcmp(strmode, "b_peripheral\n") == 0) {
+            mode = USB_MODE_B_PERIPHERAL;
         }
-        fprintf(stderr, "Match\n");
+
+        free(strmode);
     }
-    return;
+    free(path);
+
+    return mode;
 }
 
-int setup_udev(void) {
+static gint read_phy_vbus(void) {
+    const gchar* phy_sysfs_path = g_udev_device_get_sysfs_path(phy);
+    gchar* path;
+    gchar *strmode = NULL;
+    int vbus;
+
+    vbus = 0;
+
+    path = g_strconcat(phy_sysfs_path, "/vbus", NULL);
+    gboolean ok = g_file_get_contents(path, &strmode, NULL, NULL);
+    if (ok) {
+        if (strcmp(strmode, "on\n") == 0) {
+            vbus = 1;
+        } else if (strcmp(strmode, "off\n") == 0) {
+            vbus = 0;
+        }
+
+        free(strmode);
+    }
+    free(path);
+
+    return vbus;
+}
+
+static GUdevDevice* find_device(const gchar* subsystem_match, const gchar* driver_match) {
+    GList *l, *e;
+    GUdevDevice *dev, *res = NULL;
+
+    l = g_udev_client_query_by_subsystem(client, subsystem_match);
+    for (e = l; e; e = e->next) {
+        const gchar* driver;
+
+        dev = (GUdevDevice*)e->data;
+        driver = g_udev_device_get_driver(dev);
+
+        if (driver && (strcmp(driver, driver_match) == 0)) {
+            res = dev;
+            goto DONE;
+        }
+    }
+
+DONE:
+    g_list_free(l);
+
+    return res;
+}
+
+/* TODO: Improve with not just single hardcoded value */
+static GUdevDevice *find_phy(void) {
+    GUdevDevice *dev = find_device("platform", N900_PHY_DRIVER);
+    return dev;
+}
+
+static GUdevDevice *find_otg_controller(void) {
+    GUdevDevice *dev = find_device("platform", N900_USB_DRIVER);
+    return dev;
+}
+
+static int setup_udev(void) {
     const gchar *subsystems[] = { NULL};
     client = g_udev_client_new(subsystems);
     if (!client)
         return 1;
 
-    g_signal_connect(client, "uevent", G_CALLBACK(on_uevent), NULL);
+    return 0;
+}
+
+static int find_devices(void) {
+    phy = find_phy();
+    if (!phy)
+        return 1;
+
+    otg = find_otg_controller();
+    if (!otg)
+        return 1;
+
+    cache.connected = read_phy_vbus();
+    cache.usb_mode = read_usb_mode();
 
     return 0;
 }
 
+static void on_uevent(GUdevClient *client, const char *action, GUdevDevice *device) {
+    (void)client;
+
+    const gchar* sysfs_path = g_udev_device_get_sysfs_path(device);
+    const gchar* phy_sysfs_path;
+
+    phy_sysfs_path = g_udev_device_get_sysfs_path(phy);
+
+    if (strcmp(phy_sysfs_path, sysfs_path) == 0) {
+        cache.connected = (strcmp(action, "online") == 0);
+
+        /* FIXME: Schedule a callback to read usb mode and then call
+         * user_callback (instead of usleep(2) */
+        usleep(1000 * 100);
+
+        cache.usb_mode = read_usb_mode();
+        fprintf(stderr, "Connected: %d, usb_mode: %d\n", cache.connected, cache.usb_mode);
+
+
+        if ((user_callback)) {
+            user_callback(cache.connected, cache.usb_mode, user_data);
+        }
+    }
+    return;
+}
+
 int uh_init() {
+    int ret = 1;
     client = NULL;
     user_data = NULL;
     user_callback = NULL;
 
-    return setup_udev();
+    ret = setup_udev();
+    if (ret) {
+        fprintf(stderr, "setup_udev failed\n");
+        return ret;
+    }
+    ret = find_devices();
+    if (ret) {
+        fprintf(stderr, "find_devices failed\n");
+        return ret;
+    }
+
+    g_signal_connect(client, "uevent", G_CALLBACK(on_uevent), NULL);
+
+    return ret;
 }
 
 int uh_destroy() {
@@ -92,8 +215,9 @@ void uh_set_callback(UhCallback cb, gpointer data) {
     return;
 }
 
-gboolean uh_query_state() {
-    return cache.connected;
+void uh_query_state(gboolean* cable_connected, gint* usb_mode) {
+    *cable_connected = cache.connected;
+    *usb_mode = cache.usb_mode;
 }
 
 /* TODO */
@@ -102,6 +226,9 @@ char *uh_get_device_name() {
 }
 
 #if 0
+/* TODO: g_udev_device_has_sysfs_attr can be used to see if something has attrs
+ * like vbus and mode */
+
 static void test_callback(gboolean cable_connected, gpointer data) {
     fprintf(stderr, "test_callback: %d - %p.\n", cable_connected, data);
 }
@@ -109,11 +236,13 @@ static void test_callback(gboolean cable_connected, gpointer data) {
 static int main_loop(void) {
     static GMainLoop *loop = NULL;
 
-    setup_udev();
+    uh_init();
     uh_set_callback((UhCallback)test_callback, (void*)42);
 
     loop = g_main_loop_new(NULL, FALSE);
     g_main_loop_run(loop);
+
+    uh_destroy();
 
     return 0;
 }
--- udev-helper.h
+++ udev-helper.h
@@ -1,16 +1,25 @@
 #ifndef __UDEV_HELPER_H__
 #define __UDEV_HELPER_H__
-typedef void (*UhCallback)(gboolean, gpointer);
+typedef void (*UhCallback)(gboolean, gint, gpointer);
 
 
 int uh_init();
 int uh_destroy();
 void uh_set_callback(UhCallback cb, gpointer data);
-gboolean uh_query_state();
+void uh_query_state(gboolean*, gint*);
 
-/* TODO */
+/* TODO: Implement this */
 char *uh_get_device_name();
 
-
+enum {
+    USB_MODE_UNKNOWN = 0,
+    USB_MODE_B_IDLE = 1,
+    USB_MODE_B_PERIPHERAL = 2,
+    /*
+    TODO: Check on droid4/lime2
+    USB_MODE_A_IDLE,
+    USB_MODE_A_PERIPHERAL,
+    */
+};
 
 #endif /* __UDEV_HELPER_H__ */
